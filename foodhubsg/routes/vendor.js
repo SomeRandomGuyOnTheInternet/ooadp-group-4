67d5db1f233b03b999a8919ef65e4bce616e5bf1
Megan Tee <183678E@mymail.nyp.edu.sg>
2019-05-21 15:20:30 +0800

    vendor_admin

--- foodhubsg/routes/vendor.js
+++ foodhubsg/routes/vendor.js
@@ -3,3 +3,11 @@ const router = express.Router();
 const loggedIn = require('../helpers/loggedIn');
 const foodItems = require('../models/FoodItems');
 const Shops = require('../models/Shops');
+
+router.get('/showShops', loggedIn, (req, res) => {
+    res.render('vendor/vendor_index', {
+        user: req.user,
+    })
+});
+
+module.exports = router;
