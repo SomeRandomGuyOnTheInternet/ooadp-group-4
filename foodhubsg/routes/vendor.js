dea11a087b85dd0bef84d9575de96403162545ab
Megan Tee <183678E@mymail.nyp.edu.sg>
2019-05-22 21:15:43 +0800

    fixed minor bugs

--- foodhubsg/routes/vendor.js
+++ foodhubsg/routes/vendor.js
@@ -11,7 +11,7 @@ router.get('/showShops', loggedIn, (req, res) => {
 });
 
 router.get('/addShops', loggedIn, (req, res)=> { 
-    res.render('/vendors/add_newshops', { 
+    res.render('vendors/add_newshops', { 
         user: req.user, 
     })
 })
