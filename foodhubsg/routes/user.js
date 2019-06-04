6534e7d2942876d3122412434cb311eb7187ee00
Andrea <183308P@mymail.nyp.edu.sg>
2019-06-04 11:38:06 +0800

    submit button

--- foodhubsg/routes/user.js
+++ foodhubsg/routes/user.js
@@ -178,13 +178,40 @@ router.post('/settings', loggedIn, (req, res) => {
     });
     
 router.get('/faq', loggedIn, (req, res) => {
-    res.render('user/faq', {
-        user: req.user,
+    Question.findAll({
+        order: [
+            ['createdAt', 'ASC'],
+        ],
+        raw: true
+    })
+    .then((questions) => { 
+        console.log(questions)
+        res.render('user/faq', {
+            user: req.user,
+			questions: questions
+    
     })
 });
+});
 		
+router.post('/faq', (req, res) => {
+	const isAdmin = isBanned = isVendor = false;
+    const isAnswered = false;
+    let question = req.body.question;
+	var error;
 
-
+	Question.create({
+		
+		UserId: req.user.id,
+		question: question,
+			
+		}).then((questions) => {
+              
+			req.flash('success', 'You have successfully created a question!');
+            res.redirect('/user/faq');            
+           //spam check
+        })
+    });
 
 router.post('/foodJournal', loggedIn, (req, res) => {
     var searchDate = req.body.searchDate;
