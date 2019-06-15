d1d9dce64f3e4a9d3792d186c9289a218ba68b44
Vignesh <bala12rupesh@gmail.com>
2019-06-15 16:31:09 +0800

    IOB HAEY

--- foodhubsg/helpers/getShopRating.js
+++ foodhubsg/helpers/getShopRating.js
@@ -6,14 +6,13 @@ function getShopRating(foodItems) {
     }
 
     averageCalories = (totalCalories / foodItems.length);
-    console.log(averageCalories)
 
     if (averageCalories > 800) { rating = 1 } 
-    else if (700 < averageCalories < 799) { rating = 2 } 
-    else if (600 < averageCalories < 699) { rating = 3 } 
-    else if (500 < averageCalories < 599) { rating = 4 } 
+    else if (700 < averageCalories && averageCalories < 799) { rating = 2 } 
+    else if (600 < averageCalories && averageCalories < 699) { rating = 3 } 
+    else if (500 < averageCalories && averageCalories < 599) { rating = 4 } 
     else { rating = 5 }
-    
+
     return rating;
 };
 
