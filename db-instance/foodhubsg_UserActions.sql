7b2da349eb44807598f1a6ac1b84e8aaeaf3e6d8
Vignesh <bala12rupesh@gmail.com>
2019-07-21 21:19:41 +0800

    BLOWN OUT BASE

--- db-instance/foodhubsg_UserActions.sql
+++ db-instance/foodhubsg_UserActions.sql
@@ -36,7 +36,7 @@ CREATE TABLE `UserActions` (
   PRIMARY KEY (`id`),
   KEY `UserId` (`UserId`),
   CONSTRAINT `useractions_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `UserActions` (
 
 LOCK TABLES `UserActions` WRITE;
 /*!40000 ALTER TABLE `UserActions` DISABLE KEYS */;
-INSERT INTO `UserActions` VALUES (1,'gained 100 points','adding a recommended food item to your log','positive','Keep it up!',NULL,1,'2019-07-21 04:32:35','2019-07-21 04:32:36',10);
+INSERT INTO `UserActions` VALUES (1,'gained 100 points','adding a recommended food item to your log','positive','Keep it up!',NULL,1,'2019-07-21 04:32:35','2019-07-21 04:32:36',10),(2,'gained 800 points','the dev\'s need for bug testing','positive','Good job, I guess.',NULL,1,'2019-07-21 04:32:35','2019-07-21 13:15:58',10);
 /*!40000 ALTER TABLE `UserActions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-21 12:37:03
+-- Dump completed on 2019-07-21 21:16:51
