bdee1efa0ec2a342ba8b1ced23cda6abc3be9248
Vignesh <bala12rupesh@gmail.com>
2019-08-02 00:25:10 +0800

    Hermano

--- foodhubsg/helpers/sendEmail.js
+++ foodhubsg/helpers/sendEmail.js
@@ -2,16 +2,16 @@ const sgMail = require('@sendgrid/mail');
 
 
 function sendEmail(sender, email) {
-    sgMail.setApiKey("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");
+    sgMail.setApiKey("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");
 
     const message = {
         to: email,
         from: 'FoodHub.SG <admin@foodhubsg.com>',
         subject: `Invitation From ${sender.name}`,
         text: ' ',
-        html: `<img src="../public/images/healthhub-logo.png" width="65px" height="60px" alt="FoodHub.SG Logo">
+        html: `<img src="https://i.imgur.com/qeSfYXs.png" width="65px" height="60px" alt="FoodHub.SG Logo">
                 <h4>FoodHub.SG</h4><br><br>
-                Your friend, ${sender.name}, has invited you to FoodHub.SG, where you can log your calorie intake and compare your intake with other people
+                ${sender.name} has invited you to FoodHub.SG, where you can log your calorie intake and compare your intake with other people. 
                 Click here to <a href="http://127.0.0.1:5000"> <strong>register</strong></a> with us and use this referral code <strong>${sender.refCode}</strong> to 
                 start your journey!`
     };
