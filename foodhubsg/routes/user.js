b89935d26b50d5576fb08143448370cbe6ec48dd
Andrea <183308P@mymail.nyp.edu.sg>
2019-08-04 17:18:58 +0800

    made some changes

--- foodhubsg/routes/user.js
+++ foodhubsg/routes/user.js
@@ -729,7 +729,7 @@ router.post('/editQuestion',  isUser, async (req, res) => {
 	}, {
 		where: {
 			UserId: req.user.id
-		}
+        }
 	}).then(() => {
         req.flash('success', 'You have suggested an answer!');
 	res.redirect('/user/faq'); 
