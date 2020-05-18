923403c6a4f0a5a95e6d1b34bafeb0bb1d1f96d1
Vignesh <33924596+SomeRandomGuyOnTheInternet@users.noreply.github.com>
2020-05-18 15:17:22 +0800

    Create db.js

---
+++ foodhubsg/config/db.js
@@ -0,0 +1,15 @@
+// UNCOMMENT THE BELOW CODE TO REMOVE THE SEQUELIZE PASSWORD ERROR AND COMMENT OUT THE ONE BELOW THE COMMENTED CODE
+
+// module.exports = {
+//     host: 'localhost',
+//     database: 'foodhubsg',
+//     username: 'itp211',
+//     password: 'Itp2112019'
+// }
+
+module.exports = {
+    HOST: 'us-cdbr-east-06.cleardb.net',
+    USER: 'b2c1786c8469bd',
+    PASSWORD: 'f52d9cc2',
+    DB: 'heroku_c70c9870c641506',
+}
